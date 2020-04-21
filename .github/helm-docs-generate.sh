16043433c8d3e46f1b3b18a8accdd00ff31d8cf4
jubeergon <58738040+jubeergon@users.noreply.github.com>
2020-04-21 11:56:08 +0200

    Update helm-docs-generate.sh

--- .github/helm-docs-generate.sh
+++ .github/helm-docs-generate.sh
@@ -6,3 +6,6 @@ echo -e "\033[0;31m ######## The helm-docs validation failed. ######## \033[0m" 
 echo -e "\033[0;31m Make sure that the documentation has been updated. \033[0m" >&2
 
 ./helm-docs
+git add .
+git commit -m "Automated README generation"
+git push
