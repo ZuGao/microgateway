bf640a69a02289bdd6608aee638d7d12ed3880e6
jubeergon <julian.bersnak@ergon.ch>
2020-04-21 14:12:36 +0200

    HelmDocs

--- .github/helm-docs-generate.sh
+++ .github/helm-docs-generate.sh
@@ -5,7 +5,15 @@ set -euo pipefail
 echo -e "\033[0;31m ######## The helm-docs validation failed. ######## \033[0m" >&2
 echo -e "\033[0;31m Make sure that the documentation has been updated. \033[0m" >&2
 
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+          
 ./helm-docs
-git add .
+
+git checkout  ${BRANCH_NAME}
+git add charts/microgateway/README.md
 git commit -m "Automated README generation"
-git push
+echo "Push to ${BRANCH_NAME}"
+git push "https://$GITHUB_ACTOR:$CR_TOKEN@github.com/libuweber/microgateway.git" ${BRANCH_NAME}
+
+rm helm-docs
