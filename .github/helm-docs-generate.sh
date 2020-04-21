cbfb93a65240d61fb5cb0f3c4f2be9f8c4bf515c
jubeergon <58738040+jubeergon@users.noreply.github.com>
2020-04-21 11:00:43 +0200

    Update helm-docs-generate.sh

--- .github/helm-docs-generate.sh
+++ .github/helm-docs-generate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# Write Error Message
+echo -e "\033[0;31m ######## The helm-docs validation failed. ######## \033[0m" >&2
+echo -e "\033[0;31m Make sure that the documentation has been updated. \033[0m" >&2
+
 HELM_DOCS_VERSION="0.12.0"
 ./helm-docs
