a47372800692fa00d246c794eeb5d9a0effb327a
jubeergon <58738040+jubeergon@users.noreply.github.com>
2020-04-21 10:33:03 +0200

    Update helm-docs.sh

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -10,3 +10,10 @@ tar -xf /tmp/helm-docs.tar.gz helm-docs
 # validate docs
 ./helm-docs
 git diff --exit-code
+
+if [ $(git diff --exit-code) -ne 0 ]
+then
+  echo "ok"
+fi
+echo "Some Test Echo"
+echo "After the code exited"
